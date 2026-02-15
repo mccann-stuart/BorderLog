61cd95286f05321711a38779f295300780a097f2
mccann-stuart <125799576+mccann-stuart@users.noreply.github.com>
2026-02-15 21:10:43 +0200

    feat: Initialize a new SwiftUI application with SwiftData for basic item management.

---
+++ LearnTests/LearnTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnTests.swift
+//  LearnTests
+//
+//  Created by Mccann Stuart on 13/02/2026.
+//
+
+import Testing
+@testable import Learn
+
+struct LearnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
